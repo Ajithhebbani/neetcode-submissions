28d29829cbd12a16a1971fa2f1c5ead62608fb6d
AjithHebbani <ajjunag@gmail.com>
2026-09-07 22:57:05 -0500

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+         ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            // Slow moves one step
+            slow = slow.next;
+
+            // Fast moves two steps
+            fast = fast.next.next;
+
+            // They meet if there is a cycle
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
